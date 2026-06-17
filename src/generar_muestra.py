d29ac43cd5ee8fcfb9bbe138a4d118ee640fa8e3
Patricio Núñez <p.nuezcarreo@uandresbello.edu>
2026-06-16 20:40:50 -0400

    build: se crea variable que controla la cantidad de registros para la muestra

--- src/generar_muestra.py
+++ src/generar_muestra.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import os
 
+# Ajustar numero de muestras a extraer
+numero_muestras = 100000
+
 # Ajusta el nombre del archivo raw original según como lo tengas en tu equipo
 ruta_raw_original = '../data/raw/cybersecurity_threat_detection_logs.csv' 
-ruta_raw_muestra = '../data/raw/cybersecurity_threat_detection_logs_muestra_500k.csv'
+ruta_raw_muestra = f'../data/raw/cybersecurity_threat_detection_logs_muestra_{numero_muestras}.csv'
 
 print(f"Extrayendo muestra desde: {ruta_raw_original}...")
 
 try:
-    # Leemos solo las primeras 500.000 filas
-    df_muestra_raw = pd.read_csv(ruta_raw_original, nrows=500000)
+    # Leemos solo las primeras {numero_muestras} del dataset raw original para crear la muestra
+    df_muestra_raw = pd.read_csv(ruta_raw_original, nrows=numero_muestras)
     
     # Guardamos el nuevo archivo crudo que sí subiremos a GitHub
     df_muestra_raw.to_csv(ruta_raw_muestra, index=False)
